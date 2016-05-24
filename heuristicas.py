27fcc7862195aa1e4a922c90e2a025ac1d871cfe
Axel-Cab <axel.cabrera101@alu.ulpgc.es>
2016-05-24 22:25:57 +0100

    Add files via upload

--- heuristicas.py
+++ heuristicas.py
@@ -1,31 +1,89 @@
 import random
-def h0(state):
-    return random.randint(-100,100)
-def h1(state):
-    board = state.board
-    return 1
+import utils
 
 
-def display(self, state):
-    board = state.board
-    for y in range(self.v, 0, -1):
-        for x in range(1, self.h+1):
-            print board.get((x, y), '.'),
-        print
-    print "-------------------"
-    for n in range(1, self.h+1):
-        print n,
+def memoize(f):
+    memo = {}
 
-def k_in_row(self, board, move, player, (delta_x, delta_y)):
-    "Return true if there is a line through move on board for player."
+    def helper(state, player):
+        iden = hash(frozenset(state.board.items()))
+        if iden not in memo:
+            memo[iden] = f(state, player)
+        return memo[iden]
+    return helper
+
+
+def h0():
+    return random.randint(-100, 100)
+
+
+@memoize
+def h1(state, player, h=7, v=6, k=4):
+    if player == 'X':
+        enemy = 'O'
+    else:
+        enemy = 'X'
+
+    if state.utility != 0:
+        if player == 'X':
+            return utils.infinity * state.utility
+        return -(utils.infinity * state.utility)
+
+    legal = legal_moves(state)
+    board = state.board.copy()
+    computed = 0
+    for move in legal:
+        computed += compute_utility(move, player, board, (h, v, k))
+        computed -= compute_utility(move, enemy, board, (h, v, k))
+    return computed
+
+
+def compute_utility(move, player, board, (h, v, k)):
+    acum = k_in_row(board, move, player, (0, 1), (h, v, k))
+    acum += k_in_row(board, move, player, (1, 0), (h, v, k))
+    acum += k_in_row(board, move, player, (1, -1), (h, v, k))
+    acum += k_in_row(board, move, player, (1, 1), (h, v, k))
+    return acum
+
+
+def k_in_row(board, move, player, (delta_x, delta_y), (h, v, k)):
+    if player == 'X':
+        enemy = 'O'
+    else:
+        enemy = 'X'
+    pos = 0
+    computed = 0
+
+    t_pos, t_computed = count(board, (delta_x, delta_y), player, enemy, move, (h, v))
+    pos += t_pos
+    computed += t_computed
+
+    t_pos, t_computed = count(board, (-delta_x, -delta_y), player, enemy, move, (h, v))
+    pos += t_pos
+    computed += t_computed
+
+    if pos-1 < k:
+        return 0
+
+    return computed-1
+
+
+def count(board, (delta_x, delta_y), player, enemy, move, (h, v)):
+    computed = 0
+    pos = 0
     x, y = move
-    n = 0 # n is number of moves in row
-    while board.get((x, y)) == player:
-        n += 1
+    while board.get((x, y)) != enemy:
+        if not (0 < x <= h) or not (0 < y <= v):
+            break
+        if board.get((x, y)) == player:
+            computed += 5
+        else:
+            computed += 0.5
         x, y = x + delta_x, y + delta_y
-    x, y = move
-    while board.get((x, y)) == player:
-        n += 1
-        x, y = x - delta_x, y - delta_y
-    n -= 1 # Because we counted move itself twice
-    return n >= self.k
+        pos += 1
+    return pos, computed
+
+
+def legal_moves(state):
+    return [(x, y) for (x, y) in state.moves
+            if y == 1 or (x, y - 1) in state.board]
