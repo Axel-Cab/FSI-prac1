b2d05e19293479e703b80d8843c4a7eb3c92c696
supercoco1994 <supercoco1994@users.noreply.github.com>
2016-05-24 23:23:04 +0100

    Add files via upload

--- run.py
+++ run.py
@@ -1,37 +1,44 @@
 import games
 import heuristicas
 #game = games.TicTacToe(h=3,v=3,k=3)
-game = games.ConnectFour()
 
-state = game.initial
 mod_game = raw_input("Pulsa 1 para jugar contra la maquina ,2 para jugar humano contra humano y 3 para ver jugar a la maquina: ")
 
 if mod_game == '1':
     mod_dif= raw_input("Pulsa 1 para jugar dificultad dificil  ,2 para jugar dificultad media y 3 para jugar facil : ")
     select_player = raw_input("Pulse 1 para que empieze la maquina, pulse otro numero para que juege usted primero :")
+
     if select_player == '1':
         player = 'X'
+        game = games.ConnectFour(player1=player)
+        state = game.initial
     else:
         player = 'O'
-def maquina(state):
-    print "Thinking..."
-    # move = games.minimax_decision(state, game)
-    # move = games.alphabeta_full_search(state, game)
+        game = games.ConnectFour(player1=player)
+        state = game.initial1
 
-    if mod_game == '1':
-        if mod_dif == '3':
-            move = games.alphabeta_search(state, game, eval_fn=heuristicas.final)
-            state = game.make_move(move, state)
-        if mod_dif == '2':
-            move = games.alphabeta_search(state, game, eval_fn=heuristicas.final)
-            state = game.make_move(move, state)
-        else:
-            move = games.alphabeta_search(state, game, eval_fn=heuristicas.final)
-            state = game.make_move(move, state)
-    else:
-        move = games.alphabeta_search(state, game, eval_fn=heuristicas.final)
+
+if mod_game == '3':
+    mod_dif = raw_input("Pulsa 1 para maquina1 dificultad dificil  ,2 para maquina1 dificultad media y 3 para maquina1 facil : ")
+    mod_dif2 = raw_input("Pulsa 1 para maquina2 dificultad dificil  ,2 para maquina2 dificultad media y 3 para maquina2 facil : ")
+
+
+
+
+def maquina(state,player,dif):
+    print "Thinking..."
+    if dif == '1':
+        move = games.alphabeta_search(state, game, d=5, eval_fn=heuristicas.h1,player=player)
         state = game.make_move(move, state)
+    elif dif == '2':
+        move = games.alphabeta_search(state, game, d=4, eval_fn=heuristicas.h1,player=player)
+        state = game.make_move(move, state)
+    else:
+        move = games.alphabeta_search(state, game, eval_fn=heuristicas.h0,player=player)
+        state = game.make_move(move, state)
+
     return state
+
 def humano(state):
     col_str = raw_input("Movimiento: ")
     coor = int(str(col_str).strip())
@@ -46,7 +53,6 @@ def humano(state):
     return state
 
 
-
 if mod_game == '1':
     while True:
         print "Jugador a mover:", game.to_move(state)
@@ -56,7 +62,7 @@ if mod_game == '1':
             state=humano(state)
             player = 'X'
         else:
-            state=maquina(state)
+            state=maquina(state,player,mod_dif)
             player = 'O'
         print "-------------------"
         if game.terminal_test(state):
@@ -64,6 +70,9 @@ if mod_game == '1':
             print "Final de la partida"
             break
 if mod_game == '2':
+    game = games.ConnectFour(jugador='X')
+    state = game.initial
+    player = 'X'
     while True:
          print "Jugador a mover:", game.to_move(state)
          game.display(state)
@@ -81,15 +90,18 @@ if mod_game == '2':
             break
 
 if mod_game == '3':
+    game = games.ConnectFour(jugador='X')
+    state = game.initial
+    player = 'X'
     while True:
         print "Jugador a mover:", game.to_move(state)
         game.display(state)
 
         if player == 'O':
-            state = maquina(state)
+            state = maquina(state,player,mod_dif)
             player = 'X'
         else:
-            state = maquina(state)
+            state = maquina(state,player,mod_dif2)
             player = 'O'
         print "-------------------"
         if game.terminal_test(state):
@@ -104,83 +116,3 @@ if mod_game > '3':
 
 
 
-
-'''
-player = 'X'
-if mod_game == '1':
-    while True:
-        print "Jugador a mover:", game.to_move(state)
-        game.display(state)
-
-        if player == 'O':
-            col_str = raw_input("Movimiento: ")
-            coor = int(str(col_str).strip())
-            x = coor
-            y = -1
-            legal_moves = game.legal_moves(state)
-            for lm in legal_moves:
-                if lm[0] == x:
-                    y = lm[1]
-
-            state = game.make_move((x, y), state)
-            player = 'X'
-        else:
-            print "Thinking..."
-        #move = games.minimax_decision(state, game)
-        #move = games.alphabeta_full_search(state, game)
-            move = games.alphabeta_search(state, game, eval_fn=heuristica.final)
-
-            state = game.make_move(move, state)
-            player = 'O'
-        print "-------------------"
-        if game.terminal_test(state):
-            game.display(state)
-            print "Final de la partida"
-            break
-else:
-    if mod_game == '2':
-        while True:
-            print "Jugador a mover:", game.to_move(state)
-            game.display(state)
-            col_str = raw_input("Movimiento: ")
-            coor = int(str(col_str).strip())
-            x = coor
-            y = -1
-            legal_moves = game.legal_moves(state)
-            for lm in legal_moves:
-                if lm[0] == x:
-                    y = lm[1]
-
-            state = game.make_move((x, y), state)
-            if player == 'X':
-                player = 'O'
-            else:
-                player = 'X'
-            print "-------------------"
-            if game.terminal_test(state):
-                game.display(state)
-                print "Final de la partida"
-                break
-    else:
-        if mod_game == '3':
-            while True:
-                print "Jugador a mover:", game.to_move(state)
-                game.display(state)
-
-
-                print "Thinking..."
-                # move = games.minimax_decision(state, game)
-                # move = games.alphabeta_full_search(state, game)
-                move = games.alphabeta_search(state, game, eval_fn=heuristica.final)
-
-                state = game.make_move(move, state)
-                player = 'O'
-                print "-------------------"
-                if game.terminal_test(state):
-                    game.display(state)
-                    print "Final de la partida"
-                    break
-        else:
-            print "numero no valido"
-
-'''
